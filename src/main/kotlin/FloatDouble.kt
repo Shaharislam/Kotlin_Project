bcf374dc9e5ea5754855ab7c2a585f785299ab22
Rakibul Shahar <shaharislam@gmail.com>
2023-07-26 08:22:18 +0600

    Update code

--- src/main/kotlin/FloatDouble.kt
+++ src/main/kotlin/FloatDouble.kt
@@ -1,6 +1,7 @@
 fun main(){
     val floatNumber = 2F
     val doubleNumber = 2.0
+    //val doubleNumber: Double = 2.0
     val range = Float.MAX_VALUE
 
     println("Float Number:= $floatNumber and float number range:= $range ")
