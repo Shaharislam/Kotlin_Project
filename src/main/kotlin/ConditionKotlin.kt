1d7ab4c779df451d34c43596332d21adbe2c42c9
Rakibul Shahar <shaharislam@gmail.com>
2023-07-26 10:44:51 +0600

    Control Statement

---
+++ src/main/kotlin/ConditionKotlin.kt
@@ -0,0 +1,34 @@
+fun main(){
+   var x =0
+
+    val isActive = true
+
+//    if(isActive == true){
+//        println("This condition is true")
+//    }
+//    else
+//        println("This condition is false")
+
+
+    if(isActive){
+        println("This condition is true")
+    }
+    else
+        println("This condition is false")
+
+    var number = 5
+
+    if (number >=5){
+        println("Number is equal or greater than 5")
+    }else if (number>=4){
+        println("Number is equal or greater than 4")
+    }else
+    println("Number is not matching")
+
+    var value = 4000
+
+    if(isActive && number ==5 && value == 4000){
+        println("You are a next level")
+    }else
+        println("You are in same level")
+}
