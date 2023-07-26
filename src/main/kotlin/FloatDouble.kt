5dd7e4ce1be3d8719f1f353de9510190f37bb6f0
Rakibul Shahar <shaharislam@gmail.com>
2023-07-26 08:14:05 +0600

    FloatDouble Number

---
+++ src/main/kotlin/FloatDouble.kt
@@ -0,0 +1,8 @@
+fun main(){
+    val floatNumber = 2F
+    val doubleNumber = 2.0
+    val range = Float.MAX_VALUE
+
+    println("Float Number:= $floatNumber and float number range:= $range ")
+    println("Double Number:= $doubleNumber")
+}
